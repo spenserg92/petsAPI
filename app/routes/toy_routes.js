52085c585f3210102bb1fd66f08ffd91ea030ea9
spenserg92 <spenserg92@gmail.com>
2024-02-07 13:01:33 -0500

    fixes error in toy_routes

--- app/routes/toy_routes.js
+++ app/routes/toy_routes.js
@@ -1,11 +1,11 @@
+// import dependencies
 const express = require('express')
 const passport = require('passport')
 
-// pull in Mongoose model for examples
+// pull in Mongoose model for pets
 const Pet = require('../models/pet')
 
 const customErrors = require('../../lib/custom_errors')
-
 const handle404 = customErrors.handle404
 const requireOwnership = customErrors.requireOwnership
 const removeBlanks = require('../../lib/remove_blank_fields')
@@ -13,47 +13,45 @@ const requireToken = passport.authenticate('bearer', { session: false })
 
 const router = express.Router()
 
-///////////////
-//routes go here
-///////////
+///////////////////////////////////////////////////////
+// routes go here 
+///////////////////////////////////////////////////////
 
 // CREATE
-// POST /toys/jojj004030303od
+// POST /toys/oiu32458098sdf903rjknew
 router.post('/toys/:petId', removeBlanks, (req, res, next) => {
 	// save the toy from the request body
     const toy = req.body.toy
-    //save the petId for easy ref
+    // save the petId for easy ref
     const petId = req.params.petId
 
 	Pet.findById(petId)
 		// make sure we have a pet
-		.then(handle404)
-        .then((pet) => {
+        .then(handle404)
+		.then((pet) => {
             pet.toys.push(toy)
-			return pet.save
+
+			return pet.save()
 		})
-        .then(pet => res.status(201).json({pet:pet}))
-		// if an error occurs, pass it off to our error handler
-		// the error handler needs the error message and the `res` object so that it
-		// can send an error message back to the client
+        .then(pet => res.status(201).json({ pet: pet }))
 		.catch(next)
 })
 
 // UPDATE
-// PATCH /toys/5a7db6c74d55bc51bdf39793/6561bsdfuag161
+// PATCH /toys/5a7db6c74d55bc51bdf39793/546sf456dfs645dfs4894sdf4
 router.patch('/toys/:petId/:toyId', requireToken, removeBlanks, (req, res, next) => {
 	// let's grab both ids from req.params
-	const {petId, toyId} = req.params
+    const { petId, toyId } = req.params
 
 	Pet.findById(petId)
 		.then(handle404)
 		.then((pet) => {
 			// single out the toy
             const theToy = pet.toys.id(toyId)
-			//it will throw an error if the current user isn't the owner
+			// it will throw an error if the current user isn't the owner
 			requireOwnership(req, pet)
 
-			//update the existing toy
+            // update the existing toy
             theToy.set(req.body.toy)
 
 			// pass the result of Mongoose's `.update` to the next `.then`
@@ -66,10 +64,10 @@ router.patch('/toys/:petId/:toyId', requireToken, removeBlanks, (req, res, next)
 })
 
 // DESTROY
-// DELETE /toys/5a7db6c74d55bc51bdf39793/6561bsdfuag161
+// DELETE /toys/5a7db6c74d55bc51bdf39793/546sf456dfs645dfs4894sdf4
 router.delete('/toys/:petId/:toyId', requireToken, removeBlanks, (req, res, next) => {
 	// let's grab both ids from req.params
-	const {petId, toyId} = req.params
+    const { petId, toyId } = req.params
 
 	Pet.findById(petId)
 		.then(handle404)
@@ -78,8 +76,8 @@ router.delete('/toys/:petId/:toyId', requireToken, removeBlanks, (req, res, next
             const theToy = pet.toys.id(toyId)
 			// it will throw an error if the current user isn't the owner
 			requireOwnership(req, pet)
-			
-			// update the existing toy
+
+            // update the existing toy
             theToy.deleteOne()
 
 			// pass the result of Mongoose's `.update` to the next `.then`
